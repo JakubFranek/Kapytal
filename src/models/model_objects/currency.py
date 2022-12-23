1c96675f82d87216606c4760f825ce72ad3187a3
Jakub Franek <j.franek95@gmail.com>
2022-12-23 14:56:17 +0100

    Add CashAmount to currency.py

--- src/models/model_objects/currency.py
+++ src/models/model_objects/currency.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from functools import total_ordering
+
 from src.models.mixins.datetime_created_mixin import DatetimeCreatedMixin
 
 
@@ -23,3 +26,44 @@ class Currency(DatetimeCreatedMixin):
 
     def __repr__(self) -> str:
         return f"Currency('{self.code}')"
+
+
+@total_ordering
+class CashAmount:
+    """An immutable object comprising of Decimal value and a Currency."""
+
+    def __init__(self, value: Decimal, currency: Currency) -> None:
+        if not isinstance(value, Decimal):
+            raise TypeError("CashAmount.value must be a Decimal.")
+        if not value.is_finite() or value < 0:
+            raise ValueError("CashAmount.value must be finite and non-negative.")
+        self._value = value
+        if not isinstance(currency, Currency):
+            raise TypeError("CashAmount.currency must be a Currency.")
+        self._currency = currency
+
+    @property
+    def value(self) -> Decimal:
+        return self._value
+
+    @property
+    def currency(self) -> Currency:
+        return self._currency
+
+    def __repr__(self) -> str:
+        return f"CashAmount({self._value} {self._currency.code})"
+
+    def __str__(self) -> str:
+        return f"{self._value} {self._currency.code}"
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, CashAmount):
+            return NotImplemented
+        return self.currency == __o.currency and self.value == __o.value
+
+    def __lt__(self, __o: object) -> bool:
+        if not isinstance(__o, CashAmount):
+            return NotImplemented
+        if self.currency != __o.currency:
+            return NotImplemented
+        return self.value < __o.value
