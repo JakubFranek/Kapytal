5e9a77bb3e3a0cb0160a4c1e5d393406aa3e097c
Jakub Franek <j.franek95@gmail.com>
2023-03-02 11:08:46 +0100

    Refactor MainPresenter, add recent files support
    Refactor MainPresenter __init__ and other methods.
    Add recent files support to MainPresenter.
    Add TODOs.

--- src/presenters/main_presenter.py
+++ src/presenters/main_presenter.py
@@ -24,6 +24,11 @@ from src.views.forms.settings_form import SettingsForm
 from src.views.forms.tag_form import TagForm
 from src.views.main_view import MainView
 
+# TODO: implement recent file menu
+# save path to a collection whenever a file is opened
+# possible to clear
+# serialize to file upon any change
+
 
 class MainPresenter:
     def __init__(self, view: MainView, app: QApplication, app_root_path: Path) -> None:
@@ -32,39 +37,172 @@ class MainPresenter:
         self._app = app
         self._app_root_path = app_root_path
 
-        # Presenter initialization
+        self._initialize_presenters()
+        self._setup_event_observers()
+        self._connect_view_signals()
+        self._initialize_recent_paths()
+
+        # File path initialization
+        self._current_file_path: Path | None = None
+        self._update_unsaved_changes(False)
+
+        logging.debug("Showing MainView")
+        self._view.show()
+
+    def _save_to_file(self, save_as: bool) -> None:
+        logging.debug("Save to file initiated")
+        try:
+            if save_as is True or self._current_file_path is None:
+                logging.debug("Asking the user for destination path")
+                file_path = self._view.get_save_path()
+                if file_path == "":
+                    logging.info(
+                        "Save to file cancelled: invalid or no file path received"
+                    )
+                    return
+                self._current_file_path = Path(file_path)
+
+            with open(self._current_file_path, mode="w", encoding="UTF-8") as file:
+                logging.debug(f"Saving to file: {self._current_file_path}")
+                json.dump(self._record_keeper, file, cls=CustomJSONEncoder)
+
+                self._update_unsaved_changes(False)
+                self._view.show_status_message(
+                    f"File saved: {self._current_file_path}", 3000
+                )
+
+                logging.info(f"File saved: {self._current_file_path}")
+            backup_json_file(self._current_file_path)
+        except Exception:
+            handle_exception()
+
+    def _load_from_file(self) -> None:
+        logging.debug("Load from file initiated")
+        try:
+            file_path = self._view.get_open_path()
+            if not file_path:
+                logging.info(
+                    "Load from file cancelled: invalid or no file path received"
+                )
+                return
+
+            self._current_file_path = Path(file_path)
+            with open(self._current_file_path, mode="r", encoding="UTF-8") as file:
+                logging.debug(f"File path received: {self._current_file_path}")
+                backup_json_file(self._current_file_path)
+
+                logging.debug(f"Loading file: {self._current_file_path}")
+                logging.disable(logging.INFO)  # suppress logging of object creation
+                record_keeper: RecordKeeper = json.load(file, cls=CustomJSONDecoder)
+                logging.disable(logging.NOTSET)  # enable logging again
+
+                self._load_record_keeper(record_keeper)
+
+                self._view.show_status_message(
+                    f"File loaded: {self._current_file_path}", 3000
+                )
+                self._update_unsaved_changes(False)
+
+                self._update_recent_paths(self._current_file_path)
+
+                logging.debug(f"Currencies: {len(record_keeper.currencies)}")
+                logging.debug(f"Exchange Rates: {len(record_keeper.exchange_rates)}")
+                logging.debug(f"Securities: {len(record_keeper.securities)}")
+                logging.debug(f"AccountGroups: {len(record_keeper.account_groups)}")
+                logging.debug(f"Accounts: {len(record_keeper.accounts)}")
+                logging.debug(f"Transactions: {len(record_keeper.transactions)}")
+                logging.debug(f"Categories: {len(record_keeper.categories)}")
+                logging.debug(f"Tags: {len(record_keeper.tags)}")
+                logging.debug(f"Payees: {len(record_keeper.tags)}")
+                logging.info(f"File loaded: {file_path}")
+        except Exception:
+            handle_exception()
+
+    def _close_file(self) -> None:
+        if self._check_for_unsaved_changes("Close File") is False:
+            return
+        logging.info("Closing File, resetting to clean state")
+        self._record_keeper = RecordKeeper()
+        self._load_record_keeper(self._record_keeper)
+        self._current_file_path = None
+        self._update_unsaved_changes(False)
+
+    def _quit(self) -> None:
+        if self._check_for_unsaved_changes("Quit") is False:
+            return
+        logging.info("Qutting")
+        self._app.quit()
+
+    def _check_for_unsaved_changes(self, operation: str) -> bool:
+        """True: proceed \n False: abort"""
+
+        if not self._unsaved_changes:
+            return True
+
+        logging.info(
+            f"Operation '{operation}' called with unsaved changes, asking the "
+            "user for instructions"
+        )
+        reply = self._view.ask_save_before_close()
+        if reply is True:
+            self._save_to_file(save_as=False)
+            if not self._unsaved_changes:
+                logging.info(f"Saved, proceeding with '{operation}'")
+                return True
+            logging.info(f"Save cancelled, aborting operation '{operation}'")
+            return False
+        if reply is False:
+            logging.info(f"Save rejected, proceeding with operation '{operation}'")
+            return True
+        logging.info(f"Operation '{operation}' cancelled")
+        return False
+
+    def _update_unsaved_changes(self, unsaved_changes: bool) -> None:
+        self._unsaved_changes = unsaved_changes
+        self._view.set_save_status(self._current_file_path, self._unsaved_changes)
+
+    def _load_record_keeper(self, record_keeper: RecordKeeper) -> None:
+        self._record_keeper = record_keeper
+        self._account_tree_presenter.load_record_keeper(record_keeper)
+        self._currency_form_presenter.load_record_keeper(record_keeper)
+        self._payee_form_presenter.load_record_keeper(record_keeper)
+        self._tag_form_presenter.load_record_keeper(record_keeper)
+        self._security_form_presenter.load_record_keeper(record_keeper)
+        self._category_form_presenter.load_record_keeper(record_keeper)
+
+    def _initialize_presenters(self) -> None:
         logging.debug("Creating AccountTreePresenter")
         self._account_tree_presenter = AccountTreePresenter(
-            view.account_tree, self._record_keeper
+            self._view.account_tree, self._record_keeper
         )
         logging.debug("Creating Currency Form and CurrencyFormPresenter")
-        currency_form = CurrencyForm(parent=view)
+        currency_form = CurrencyForm(parent=self._view)
         self._currency_form_presenter = CurrencyFormPresenter(
             currency_form, self._record_keeper
         )
         logging.debug("Creating SecurityForm and SecurityFormPresenter")
-        security_form = SecurityForm(parent=view)
+        security_form = SecurityForm(parent=self._view)
         self._security_form_presenter = SecurityFormPresenter(
             security_form, self._record_keeper
         )
         logging.debug("Creating PayeeForm and PayeeFormPresenter")
-        payee_form = PayeeForm(parent=view)
+        payee_form = PayeeForm(parent=self._view)
         self._payee_form_presenter = PayeeFormPresenter(payee_form, self._record_keeper)
         logging.debug("Creating TagForm and TagFormPresenter")
-        tag_form = TagForm(parent=view)
+        tag_form = TagForm(parent=self._view)
         self._tag_form_presenter = TagFormPresenter(tag_form, self._record_keeper)
         logging.debug("Creating CategoryForm and CategoryFormPresenter")
-        category_form = CategoryForm(parent=view)
+        category_form = CategoryForm(parent=self._view)
         self._category_form_presenter = CategoryFormPresenter(
             category_form, self._record_keeper
         )
         logging.debug("Creating SettingsForm and SettingsFormPresenter")
-        settings_form = SettingsForm(parent=view)
+        settings_form = SettingsForm(parent=self._view)
         self._settings_form_presenter = SettingsFormPresenter(
-            settings_form, app_root_path
+            settings_form, self._app_root_path
         )
 
-        # Setting up Event observers
+    def _setup_event_observers(self) -> None:
         self._account_tree_presenter.event_data_changed.append(
             lambda: self._update_unsaved_changes(True)
         )
@@ -87,7 +225,7 @@ class MainPresenter:
             lambda: self._update_unsaved_changes(True)
         )
 
-        # MainView pyqtSignal connections
+    def _connect_view_signals(self) -> None:
         self._view.signal_exit.connect(self._quit)
         self._view.signal_open_currency_form.connect(
             self._currency_form_presenter.show_form
@@ -109,127 +247,25 @@ class MainPresenter:
         self._view.signal_open_file.connect(self._load_from_file)
         self._view.signal_close_file.connect(self._close_file)
 
-        # File path initialization
-        self.current_file_path: Path | None = None
-        self._update_unsaved_changes(False)
+    def _initialize_recent_paths(self) -> None:
+        self._recent_paths_file = self._app_root_path / "saved_data/recent_files.json"
 
-        logging.debug("Showing MainView")
-        self._view.show()
+        if not self._recent_paths_file.exists():
+            self._recent_paths: list[Path] = []
+        else:
+            with open(self._recent_paths_file, mode="r", encoding="UTF-8") as file:
+                logging.debug(f"Loading recent paths: {self._recent_paths_file}")
+                paths_as_str: list[str] = json.load(file, cls=CustomJSONDecoder)
+                self._recent_paths = [Path(path) for path in paths_as_str]
 
-    def _save_to_file(self, save_as: bool) -> None:
-        logging.debug("Save to file initiated")
-        try:
-            if save_as is True or self.current_file_path is None:
-                logging.debug("Asking the user for destination path")
-                file_path = self._view.get_save_path()
-                if file_path == "":
-                    logging.info(
-                        "Save to file cancelled: invalid or no file path received"
-                    )
-                    return
-                self.current_file_path = Path(file_path)
+    def _update_recent_paths(self, new_path: Path) -> None:
+        if new_path in self._recent_paths:
+            self._recent_paths.remove(new_path)
+        self._recent_paths.insert(0, new_path)
 
-            with open(self.current_file_path, mode="w", encoding="UTF-8") as file:
-                logging.debug(f"Saving to file: {self.current_file_path}")
-                json.dump(self._record_keeper, file, cls=CustomJSONEncoder)
+        recent_paths = [str(path) for path in self._recent_paths]
+        with open(self._recent_paths_file, mode="w", encoding="UTF-8") as file:
+            logging.debug(f"Saving recent paths to file: {self._recent_paths_file}")
+            json.dump(recent_paths, file, cls=CustomJSONEncoder)
 
-                self._update_unsaved_changes(False)
-                self._view.show_status_message(
-                    f"File saved: {self.current_file_path}", 3000
-                )
-
-                logging.info(f"File saved: {self.current_file_path}")
-            backup_json_file(self.current_file_path)
-        except Exception:
-            handle_exception()
-
-    def _load_from_file(self) -> None:
-        logging.debug("Load from file initiated")
-        try:
-            file_path = self._view.get_open_path()
-            if not file_path:
-                logging.info(
-                    "Load from file cancelled: invalid or no file path received"
-                )
-                return
-
-            self.current_file_path = Path(file_path)
-            with open(self.current_file_path, mode="r", encoding="UTF-8") as file:
-                logging.debug(f"File path received: {self.current_file_path}")
-                backup_json_file(self.current_file_path)
-
-                logging.debug(f"Loading file: {self.current_file_path}")
-                logging.disable(logging.INFO)  # suppress logging of object creation
-                record_keeper: RecordKeeper = json.load(file, cls=CustomJSONDecoder)
-                logging.disable(logging.NOTSET)
-
-                self._load_record_keeper(record_keeper)
-
-                self._view.show_status_message(
-                    f"File loaded: {self.current_file_path}", 3000
-                )
-                self._update_unsaved_changes(False)
-                logging.debug(f"Currencies: {len(record_keeper.currencies)}")
-                logging.debug(f"Exchange Rates: {len(record_keeper.exchange_rates)}")
-                logging.debug(f"Securities: {len(record_keeper.securities)}")
-                logging.debug(f"AccountGroups: {len(record_keeper.account_groups)}")
-                logging.debug(f"Accounts: {len(record_keeper.accounts)}")
-                logging.debug(f"Transactions: {len(record_keeper.transactions)}")
-                logging.debug(f"Categories: {len(record_keeper.categories)}")
-                logging.debug(f"Tags: {len(record_keeper.tags)}")
-                logging.debug(f"Payees: {len(record_keeper.tags)}")
-                logging.info(f"File loaded: {file_path}")
-        except Exception:
-            handle_exception()
-
-    def _close_file(self) -> None:
-        if self._check_for_unsaved_changes("Close File") is False:
-            return
-        logging.info("Closing File, resetting to clean state")
-        self._record_keeper = RecordKeeper()
-        self._load_record_keeper(self._record_keeper)
-        self.current_file_path = None
-        self._update_unsaved_changes(False)
-
-    def _quit(self) -> None:
-        if self._check_for_unsaved_changes("Quit") is False:
-            return
-        logging.info("Qutting")
-        self._app.quit()
-
-    def _check_for_unsaved_changes(self, operation: str) -> bool:
-        """True: proceed \n False: abort"""
-
-        if self._unsaved_changes:
-            logging.info(
-                f"Operation '{operation}' called with unsaved changes, asking the "
-                "user for instructions"
-            )
-            reply = self._view.ask_save_before_close()
-            if reply is True:
-                self._save_to_file(save_as=False)
-                if not self._unsaved_changes:
-                    logging.info(f"Saved, proceeding with '{operation}'")
-                    return True
-                logging.info(f"Save cancelled, aborting operation '{operation}'")
-                return False
-            if reply is False:
-                logging.info(f"Save rejected, proceeding with operation '{operation}'")
-                return True
-            logging.info(f"Operation '{operation}' cancelled")
-            return False
-        # No unsaved changes, proceed
-        return True
-
-    def _update_unsaved_changes(self, unsaved_changes: bool) -> None:
-        self._unsaved_changes = unsaved_changes
-        self._view.set_save_status(self.current_file_path, self._unsaved_changes)
-
-    def _load_record_keeper(self, record_keeper: RecordKeeper) -> None:
-        self._record_keeper = record_keeper
-        self._account_tree_presenter.load_record_keeper(record_keeper)
-        self._currency_form_presenter.load_record_keeper(record_keeper)
-        self._payee_form_presenter.load_record_keeper(record_keeper)
-        self._tag_form_presenter.load_record_keeper(record_keeper)
-        self._security_form_presenter.load_record_keeper(record_keeper)
-        self._category_form_presenter.load_record_keeper(record_keeper)
+        # update UI
