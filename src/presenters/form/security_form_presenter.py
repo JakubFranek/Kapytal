20f565271516dd67daf4ffdbe340220359aaccc4
Jakub Franek <j.franek95@gmail.com>
2023-07-28 18:54:21 +0200

    Make Security Form Overview update on price change

--- src/presenters/form/security_form_presenter.py
+++ src/presenters/form/security_form_presenter.py
@@ -323,6 +323,7 @@ class SecurityFormPresenter:
 
         self._dialog.close()
         self._update_chart(security)
+        self.update_overview_model_data()
         self.reset_self = False
         self.event_data_changed()
         self.reset_self = True
@@ -365,6 +366,7 @@ class SecurityFormPresenter:
         if any_deleted:
             self._update_price_table_and_chart(security)
             self._price_selection_changed()
+            self.update_overview_model_data()
             self.reset_self = False
             self.event_data_changed()
             self.reset_self = True
@@ -427,6 +429,7 @@ class SecurityFormPresenter:
         )
 
         self._update_price_table_and_chart(security)
+        self.update_overview_model_data()
         self._dialog.close()
         self.reset_self = False
         self.event_data_changed()
