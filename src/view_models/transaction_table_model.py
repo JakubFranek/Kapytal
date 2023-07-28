b29c17c1b0fcecd76aec1b88da9b574e4d652aa8
Jakub Franek <j.franek95@gmail.com>
2023-07-28 18:46:24 +0200

    Fix number notation in Transaction table columns

--- src/view_models/transaction_table_model.py
+++ src/view_models/transaction_table_model.py
@@ -476,9 +476,9 @@ class TransactionTableModel(QAbstractTableModel):
     @staticmethod
     def _get_transaction_shares_string(transaction: Transaction) -> str:
         if isinstance(transaction, SecurityTransaction):
-            return f"{transaction.get_shares(transaction.security_account):,}"
+            return f"{transaction.get_shares(transaction.security_account):,f}"
         if isinstance(transaction, SecurityTransfer):
-            return f"{transaction.get_shares(transaction.recipient):,}"
+            return f"{transaction.get_shares(transaction.recipient):,f}"
         return ""
 
     @staticmethod
@@ -490,7 +490,7 @@ class TransactionTableModel(QAbstractTableModel):
     @staticmethod
     def _get_transaction_price_per_share_string(transaction: Transaction) -> str:
         if isinstance(transaction, SecurityTransaction):
-            return f"{transaction.price_per_share.value_normalized:,}"
+            return f"{transaction.price_per_share.value_normalized:,f}"
         return ""
 
     def _get_transaction_amount_string(
