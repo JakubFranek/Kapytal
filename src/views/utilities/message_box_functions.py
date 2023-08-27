eb353ee6c4abb18ac12c578d3a5612b98fef3647
Jakub Franek <j.franek95@gmail.com>
2023-08-27 12:05:43 +0200

    Improve Transaction Filter unrelated types dialog
    Add Cancel button to Transaction Filter "unrelated Transaction types
    selected" dialog.
    Add ask_yes_no_cancel_question function to message_box_functions module.

--- src/views/utilities/message_box_functions.py
+++ src/views/utilities/message_box_functions.py
@@ -21,3 +21,35 @@ def ask_yes_no_question(
             defaultButton=QMessageBox.StandardButton.No,
         )
     return answer == QMessageBox.StandardButton.Yes
+
+
+def ask_yes_no_cancel_question(
+    parent: QWidget, question: str, title: str, *, warning: bool = False
+) -> bool | None:
+    if not warning:
+        answer = QMessageBox.question(
+            parent,
+            title,
+            question,
+            buttons=QMessageBox.StandardButton.Yes
+            | QMessageBox.StandardButton.No
+            | QMessageBox.StandardButton.Cancel,
+            defaultButton=QMessageBox.StandardButton.Cancel,
+        )
+    else:
+        answer = QMessageBox.warning(
+            parent,
+            title,
+            question,
+            buttons=QMessageBox.StandardButton.Yes
+            | QMessageBox.StandardButton.No
+            | QMessageBox.StandardButton.Cancel,
+            defaultButton=QMessageBox.StandardButton.Cancel,
+        )
+    return (
+        True
+        if answer == QMessageBox.StandardButton.Yes
+        else False
+        if answer == QMessageBox.StandardButton.No
+        else None
+    )
