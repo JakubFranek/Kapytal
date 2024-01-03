e104e1920b95bb7f09569caf49be245217f70fa8
Jakub Franek <j.franek95@gmail.com>
2024-01-03 20:23:08 +0100

    Resize Transaction Table Form columns to contents

--- src/presenters/form/transaction_table_form_presenter.py
+++ src/presenters/form/transaction_table_form_presenter.py
@@ -131,13 +131,11 @@ class TransactionTableFormPresenter:
                 self._model.post_reset_model()
 
             self._update_table_columns()
-            self._form.table_view.resizeColumnsToContents()
             self._form.set_window_title(title)
-
             self._update_number_of_shown_transactions()
             self._update_selected_transactions_amount()
-
             self._form.setParent(parent, Qt.WindowType.Window)
+            self._form.table_view.resizeColumnsToContents()
             self._form.show_form()
         except:  # noqa: TRY302
             raise
